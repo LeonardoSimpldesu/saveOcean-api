dc0fd5e0b3016fa2d20eab9c5ae34a492267b1cc
Kleber777 <klebermirandalima@gmail.com>
2024-06-04 21:59:17 -0300

    Criando estruturas dos jsons datas e endpoint para consultar os conteúdos da plataforma

--- src/http/controller/example.ts
+++ src/http/controller/example.ts
@@ -4,7 +4,12 @@ import { exampleService } from "../../service/example";
 export async function example(request: FastifyRequest, reply: FastifyReply) {
     try {
       const example = exampleService(request)
-      return reply.status(200).send(example);
+      const fs = require("fs")
+      const pontuacaoData = fs.readFileSync("pontuacao.json").toString()
+      console.log(pontuacaoData); 
+
+      
+      return reply.status(200).send(JSON.parse(pontuacaoData));
     } catch (error) {
       console.log(error)
       return reply.status(409).send(error)
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify"
 import { example } from "./controller/example"
+import { allContents } from "./controller/content"
 
 export async function appRoutes(app: FastifyInstance){
     app.get('/example', example)
+    app.get("/Content/allContents", allContents)
 }
