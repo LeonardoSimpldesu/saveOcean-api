5db6480e35fe28b1ef156df64559eb719b0a55a1
Kleber777 <klebermirandalima@gmail.com>
2024-06-06 14:16:00 -0300

    Retornando endpoint para o ranking e para todas as especies do app

---
+++ src/http/controller/ranking.ts
@@ -0,0 +1,15 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function classification(request: FastifyRequest, reply: FastifyReply) {
+    try {
+
+        const fs = require("fs")
+        const contents = fs.readFileSync("ranking.json")
+        let response = JSON.parse(contents)
+
+        return reply.status(200).send(response)
+
+    } catch (error) {
+        return reply.status(409).send(error)
+    }
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify"
 import { example } from "./controller/example"
 import { allContents } from "./controller/content"
+import { allEspecies } from "./controller/aquario"
+import { classification } from "./controller/ranking"
 
 export async function appRoutes(app: FastifyInstance){
     app.get('/example', example)
     app.get("/Content/allContents", allContents)
+    app.get("/Aquario/allEspecies", allEspecies)
+    app.get("/Ranking/classification", classification)
 }
