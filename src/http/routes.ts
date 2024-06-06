e7a7c477d2656cbf4f8d6b62a0109c75701a35b7
Kleber777 <klebermirandalima@gmail.com>
2024-06-06 20:00:25 -0300

    Criando o endpoint da atividade

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,12 @@ import { example } from "./controller/example"
 import { allContents } from "./controller/content"
 import { allEspecies } from "./controller/aquario"
 import { classification } from "./controller/ranking"
+import { contentTask } from "./controller/tasks"
 
 export async function appRoutes(app: FastifyInstance){
     app.get('/example', example)
     app.get("/Content/allContents", allContents)
     app.get("/Aquario/allEspecies", allEspecies)
     app.get("/Ranking/classification", classification)
+    app.get("/Task/contentTask", contentTask)
 }
